056f4e0c1c814bef62981b82ce01019a81fed959
marchin7 <114770259+marchin7@users.noreply.github.com>
2022-11-21 18:49:37 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 
 const faqQuestions = document.querySelectorAll('.faq-question');
 
-faqQuestions.forEach(question =>{question.addEventListener('click', ()=>{
-    faqQuestions.forEach(activeQuestion =>{
-        if(question !== activeQuestion) {
-            activeQuestion.nextElementSibling.classList.remove('active')
-            activeQuestion.firstElementChild.classList.remove('rotate')
-            activeQuestion.classList.remove('active')
+
+faqQuestions.forEach(item=> {item.addEventListener('click', ()=> {
+    faqQuestions.forEach(question=> {
+        if(item !== question){
+            question.nextElementSibling.style.maxHeight = '0px'
+            question.firstElementChild.classList.remove('rotate')
+            question.classList.remove('active')
         }
-        else {
-            activeQuestion.nextElementSibling.classList.toggle('active')
-            activeQuestion.firstElementChild.classList.toggle('rotate')
-            activeQuestion.classList.toggle('active') 
+        else{
+            if(question.nextElementSibling.style.maxHeight === question.nextElementSibling.scrollHeight + 'px'){
+                question.nextElementSibling.style.maxHeight = '0px'
+            }
+            else{question.nextElementSibling.style.maxHeight = question.nextElementSibling.scrollHeight + 'px'}
+        question.firstElementChild.classList.toggle('rotate')
+        question.classList.toggle('active')
         }
     })
 })});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
