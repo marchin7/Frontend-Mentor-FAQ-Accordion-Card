e389ccae6b8a974978aa902f6a2d70e1d504437e
marchin7 <114770259+marchin7@users.noreply.github.com>
2022-11-21 18:11:50 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 
 const faqQuestions = document.querySelectorAll('.faq-question');
+const faqAnswers = document.querySelectorAll('.faq-answer');
+
+
+faqQuestions.forEach(item=> {item.addEventListener('click', ()=> {
+    faqQuestions.forEach(question=> {
+        if(item !== question){
+            question.nextElementSibling.style.maxHeight = '0px'
+            question.firstElementChild.classList.remove('rotate')
+            question.classList.remove('active')
+        }
+        else{
+            if(question.nextElementSibling.style.maxHeight === question.nextElementSibling.scrollHeight + 'px'){
+                question.nextElementSibling.style.maxHeight = '0px'
+            }
+            else{question.nextElementSibling.style.maxHeight = question.nextElementSibling.scrollHeight + 'px'}
+        question.firstElementChild.classList.toggle('rotate')
+        question.classList.toggle('active')
+        }
+    })
+})});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 faqQuestions.forEach(question =>{question.addEventListener('click', ()=>{
     faqQuestions.forEach(activeQuestion =>{
@@ -15,3 +55,5 @@ faqQuestions.forEach(question =>{question.addEventListener('click', ()=>{
         }
     })
 })});
+
+*/
