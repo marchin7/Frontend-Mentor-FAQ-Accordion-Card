2fd950868e43e9604b5a5373698e2d088b9394d0
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-02-19 20:48:17 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
+const faqQuestions = document.querySelectorAll(".faq-question");
 
-const faqQuestions = document.querySelectorAll('.faq-question');
-
-
-faqQuestions.forEach(item=> {item.addEventListener('click', ()=> {
-    faqQuestions.forEach(question=> {
-        if(item !== question){
-            question.nextElementSibling.style.maxHeight = '0px'
-            question.firstElementChild.classList.remove('rotate')
-            question.classList.remove('active')
-        }
-        else{
-            if(question.nextElementSibling.style.maxHeight === question.nextElementSibling.scrollHeight + 'px'){
-                question.nextElementSibling.style.maxHeight = '0px'
+faqQuestions.forEach((item) => {
+    item.addEventListener("click", () => {
+        faqQuestions.forEach((question) => {
+            if (item !== question) {
+                question.nextElementSibling.style.maxHeight = "0px";
+                question.firstElementChild.classList.remove("rotate");
+                question.classList.remove("active");
+            } else {
+                if (
+                    question.nextElementSibling.style.maxHeight ===
+                    question.nextElementSibling.scrollHeight + "px"
+                ) {
+                    question.nextElementSibling.style.maxHeight = "0px";
+                } else {
+                    question.nextElementSibling.style.maxHeight =
+                        question.nextElementSibling.scrollHeight + "px";
+                }
+                question.firstElementChild.classList.toggle("rotate");
+                question.classList.toggle("active");
             }
-            else{question.nextElementSibling.style.maxHeight = question.nextElementSibling.scrollHeight + 'px'}
-            
-            question.firstElementChild.classList.toggle('rotate')
-            question.classList.toggle('active')
-        }
-    })
-})});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        });
+    });
+});
