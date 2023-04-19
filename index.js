fe6efc51483b9a34f546432f0a4366e982e2d314
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-04-19 16:50:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ faqQuestions.forEach((item) => {
                 question.nextElementSibling.style.maxHeight = "0px";
                 question.firstElementChild.classList.remove("rotate");
                 question.classList.remove("active");
+                question.setAttribute("aria-expanded", "false");
             } else {
-                if (
-                    question.nextElementSibling.style.maxHeight ===
-                    question.nextElementSibling.scrollHeight + "px"
-                ) {
+                if (question.nextElementSibling.style.maxHeight === question.nextElementSibling.scrollHeight + "px") {
                     question.nextElementSibling.style.maxHeight = "0px";
+                    question.setAttribute("aria-expanded", "false");
                 } else {
-                    question.nextElementSibling.style.maxHeight =
-                        question.nextElementSibling.scrollHeight + "px";
+                    question.nextElementSibling.style.maxHeight = question.nextElementSibling.scrollHeight + "px";
+                    question.setAttribute("aria-expanded", "true");
                 }
                 question.firstElementChild.classList.toggle("rotate");
                 question.classList.toggle("active");
